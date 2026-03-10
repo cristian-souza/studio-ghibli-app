119db1d47eedfe3ca84f1338e0f5dfeb0ba88c93
Cristian Souza <cristiansouza_dev@outlook.com>
2026-03-10 16:00:38 -0400

    fix; feito a ordenação do filmes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,17 +2,21 @@ import { Link } from 'react-router-dom'
 import { useFilms } from '../../hooks/useFilm'
 
 export const Home = () => {
-    const { data, isLoading, error } = useFilms(10)
+    const { data, isLoading, error } = useFilms(20)
 
     if (isLoading) return <p>Carregando...</p>
     if (error) return <p>Erro ao carregar filmes: {error.message}</p>
 
+    const filmesOrdenados = data
+        ?.slice()
+        .sort((a, b) => a.title.localeCompare(b.title));
+
     return (
         <ul>
-            {data?.map((film) => (
+            {filmesOrdenados?.map((film) => (
                 <li key={film.id}>
                     <h2>{film.title}</h2>
-                    <Link to={`/film/${film.id}`}>
+                    <Link to={`/FilmDetail/${film.id}`}>
                         <img src={film.movie_banner} alt={film.title} />
                     </Link>
                 </li>
