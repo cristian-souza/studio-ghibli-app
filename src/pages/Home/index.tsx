e197c395377697f59e828e7914de98e71ef6512d
Cristian Souza <cristiansouza_dev@outlook.com>
2026-03-10 14:53:13 -0400

    feet: adição do hook  com tanstackquery e renderização em tela

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,21 @@
+import { useFilms } from '../../hooks/useFilm'
+
 export const Home = () => {
+    const { data, isLoading, error } = useFilms(10)
+
+    if (isLoading) return <p>Carregando...</p>
+    if (error) return <p>Erro ao carregar filmes: {error.message}</p>
+
     return (
-        <main>
-            <h1>Studio Ghibli Filmes</h1>
-        </main>
-    )
+        <ul>
+            {data?.map((film) => (
+                <li key={film.id}>
+                    <h2>{film.title}</h2>
+                    <a href="">
+                        <img src={film.movie_banner} alt={film.title} />
+                    </a>
+                </li>
+            ))}
+        </ul>
+    );
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,8 +2,21 @@ export interface Film {
     id: string;
     title: string;
     description: string;
+    image: string;
+    movie_banner: string;
     director: string;
     producer: string;
-    release_date: string;
-    rt_score: string;
+    release_date: number;
+    rt_score: number;
+}
+
+export interface newFilm {
+    title: string;
+    image: string;
+    movie_banner: string;
+    description: string;
+    director: string;
+    producer: string;
+    release_date: number;
+    rt_score: number;
 }
